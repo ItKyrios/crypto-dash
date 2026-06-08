841b509f0e7f2808fec827c4c902b1c09e2ee769
ItKyrios <kyriositsolutions@gmail.com>
2026-06-08 16:31:39 +1000

    React router installed. HomePage setup complete.

---
+++ src/pages/home.jsx
@@ -0,0 +1,77 @@
+import CoinCard from '../components/CoinCard';
+import LimitSelector from '../components/LimitSelector';
+import FilterInput from '../components/FilterInput';
+import SortSelector from '../components/SortSelector';
+
+const HomePage = ({
+  coins,
+  filter,
+  setFilter,
+  limit,
+  setLimit,
+  sortBy,
+  setSortBy,
+  loading,
+  error,
+}) => {
+  const filteredCoins = coins
+    .filter((coin) => {
+      return (
+        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(filter.toLowerCase())
+      );
+    })
+    .slice()
+    .sort((a, b) => {
+      switch (sortBy) {
+        case 'market_cap_desc':
+          return b.market_cap - a.market_cap;
+          break;
+        case 'market_cap_asc':
+          return a.market_cap - b.market_cap;
+          break;
+        case 'price_desc':
+          return b.current_price - a.current_price;
+          break;
+        case 'price_asc':
+          return a.current_price - b.current_price;
+          break;
+        case 'change_desc':
+          return b.price_change_percentage_24h - a.price_change_percentage_24h;
+          break;
+        case 'change_asc':
+          return a.price_change_percentage_24h - b.price_change_percentage_24h;
+          break;
+
+        default:
+          break;
+      }
+    });
+
+  return (
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+
+      <div className='top-controls'>
+        <FilterInput filter={filter} onFilterChange={setFilter} />
+        <LimitSelector limit={limit} onLimitChange={setLimit} />
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
+      </div>
+
+      {loading && <p>Loading...</p>}
+      {error && <div className='error'>{error}</div>}
+
+      {!loading && !error && (
+        <main className='grid'>
+          {filteredCoins.length > 0 ? (
+            filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
+          ) : (
+            <p>No matching coins</p>
+          )}
+        </main>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
