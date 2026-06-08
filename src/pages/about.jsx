dd006f3c70850d994458ac28252f22ec00ba9886
ItKyrios <kyriositsolutions@gmail.com>
2026-06-08 17:27:19 +1000

    Created AboutPage. Used Link component from React Router to add Header navigation.

---
+++ src/pages/about.jsx
@@ -0,0 +1,26 @@
+const AboutPage = () => {
+  return (
+    <div className='about'>
+      <h1>About Crypto Dash</h1>
+      <p>
+        Crypto Dash is a simple React application that displays live
+        cryptocurrency data using CoinGecko API.
+      </p>
+      <p>
+        You can explore the top cryptocurrencies by market cap, filter by name
+        or symbol, and sort them by price, market cap, or 24-hour change.
+      </p>
+      <p>
+        This project is built as a part of a React tutorial to help me
+        understand hooks, components, state management, and integrating with
+        external APIs.
+      </p>
+      <p>
+        🚀Future features might include detailed coin views, favourites,
+        pagination, and much more!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
